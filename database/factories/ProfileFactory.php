57d7b006d749b596e1a7d52136d0c8c798bec1eb
frasergrandfield <frasergrandfield110@gmail.com>
2020-11-06 00:22:29 +0000

    Created Models. Created Profile seeder.

---
+++ database/factories/ProfileFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Profile;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProfileFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Profile::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'email' => $this->faker->unique()->safeEmail,
+            'first_name' => $this->faker->firstName(),
+            'last_name' => $this->faker->lastName(),
+            'profile_picture' => $this->faker->image('/temp/', 200, 200)
+        ];
+    }
+}
---
+++ database/seeders/ProfileTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Profile;
+use Faker\Factory;
+
+class ProfileTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        
+    }
+}
